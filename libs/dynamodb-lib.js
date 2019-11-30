07fc80f9e7deebee03d25805f2b20926ff79d9aa
br3ntor <br3ntor@gmail.com>
2019-11-29 23:22:28 -0800

    Adding our Serverless API

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,10 @@
+import AWS from 'aws-sdk';
+
+// I think I need this
+AWS.config.update({ region: 'us-west-1' });
+
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
