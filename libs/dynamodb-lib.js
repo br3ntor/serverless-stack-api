a9c9bae63f2f843acee3b62036293f0d8020eeb5
br3ntor <br3ntor@gmail.com>
2020-01-26 17:58:19 -0800

    Comment out manual region update

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk';
 
 // I think I need this
-AWS.config.update({ region: 'us-west-1' });
+// AWS.config.update({ region: 'us-west-1' });
 
 export function call(action, params) {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
