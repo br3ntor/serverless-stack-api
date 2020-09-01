6921074158c86a1d89f304c3cf71906499d6dd25
br3ntor <br3ntor@gmail.com>
2020-09-01 01:50:29 -0700

    updated all dependencies

--- create.js
+++ create.js
@@ -1,6 +1,6 @@
-import uuid from 'uuid';
-import * as dynamoDbLib from './libs/dynamodb-lib';
-import { success, failure } from './libs/response-lib';
+import { v4 as uuidv4 } from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
@@ -8,15 +8,15 @@ export async function main(event, context) {
     TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
+      noteId: uuidv4(),
       content: data.content,
       attachment: data.attachment,
-      createdAt: Date.now()
-    }
+      createdAt: Date.now(),
+    },
   };
 
   try {
-    await dynamoDbLib.call('put', params);
+    await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
     return failure({ status: false });
